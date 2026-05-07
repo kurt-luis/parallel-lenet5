9c8232949c0f3234ae8d66faafa9daef980f9a6c
Kurt Luis Landoy <ks.landoy@gmail.com>
2026-05-07 20:12:26 +0800

    added second layer consisting of ReLU + maxpool1

--- src/baseline/main.cpp
+++ src/baseline/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
-#include "../../include/utils/file_io.h" // Include your new header
+#include <limits>
+#include <algorithm>
+#include "../../include/utils/file_io.h"
 
 using namespace std;
 
@@ -43,6 +45,47 @@ int main() {
         }
 
         verify_tensors(my_conv1, golden_conv1, 1e-4);
+
+        // SECOND LAYER: ReLU + Max Pool 1
+        auto golden_pool1 = load_binary("../results/predictions/golden_pool1.bin");
+
+        int PoolK = 2;      
+        int PoolS = 2;      
+        int PoolOutH = OutH / PoolS; 
+        int PoolOutW = OutW / PoolS; 
+
+        vector<float> my_pool1(N * OutC * PoolOutH * PoolOutW, 0.0f);
+
+        for (int n = 0; n < N; ++n) {
+            for (int c = 0; c < OutC; ++c) {
+                for (int ph = 0; ph < PoolOutH; ++ph) {
+                    for (int pw = 0; pw < PoolOutW; ++pw) {
+                        
+                        float max_val = -numeric_limits<float>::infinity();
+
+                        for (int kh = 0; kh < PoolK; ++kh) {
+                            for (int kw = 0; kw < PoolK; ++kw) {
+                                
+                                int ih = ph * PoolS + kh;
+                                int iw = pw * PoolS + kw;
+                                
+                                int in_idx = idx4d(n, c, ih, iw, OutC, OutH, OutW);
+                                float val = my_conv1[in_idx];
+                                
+                                val = max(0.0f, val);
+                                max_val = max(max_val, val);
+                            }
+                        }
+
+                        int out_idx = idx4d(n, c, ph, pw, OutC, PoolOutH, PoolOutW);
+                        my_pool1[out_idx] = max_val;
+                    }
+                }
+            }
+        }
+
+        cout << "Verifying Pool1...\n";
+        verify_tensors(my_pool1, golden_pool1, 1e-4);
 
     } catch (const exception& e) {
         cerr << "Error: " << e.what() << "\n";
